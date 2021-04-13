331371c3ed5042707b8695124e2c99d7effd5f5f
inneska-v <36503787+inneska-v@users.noreply.github.com>
2021-04-13 21:52:05 +0300

    Add files via upload

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,14 @@
-// вызов мобильного меню
-let menuToggle = $('.header-menu-toggle');
-menuToggle.on('click', function (event) {
-  event.preventDefault();
-  $('.header-nav').slideToggle(200);
-})
+// добавление класса active на меню в шапке с возможностью стилизировать активную ссылку
+var theThing = $('a[aria-current="page"]');
+theThing.addClass('active');
+
+// Меню бургер
+const menuToggle = document.querySelector('.mobile-menu-toggle');
+//проверка есть ли такой класс, объект
+if (menuToggle) {
+  const headerNav = document.querySelector('.grand-wrapper');
+  menuToggle.addEventListener("click", function(e) {
+    menuToggle.classList.toggle('mobile-menu-toggle-withmenu');
+    headerNav.classList.toggle('grand-wrapper-withmenu');
+  });
+}
